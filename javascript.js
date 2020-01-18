9651393982a1711ce9957621286225cb44dde5d8
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2020-01-18 12:43:02 -0500

    fix attempt number like 50-somthing

--- javascript.js
+++ javascript.js
@@ -35,14 +35,14 @@ window.addEventListener("load", function() {
     // get the garbage code the user wrote that probably does nothing interesting:
     var code = document.getElementById("code").value;
     var interval = setInterval(function() {
-      var ret = run(code, curChar);
-      if (ret !== undefined) {
-        curChar = ret;
-      }
       console.log(curChar + ": \"" + code[curChar] + "\"");
       console.log(cells);
       console.log("Pointer: " + pointer);
       console.log("---");
+      var ret = run(code, curChar);
+      if (ret !== undefined) {
+        curChar = ret;
+      }
       curChar++;
       if (curChar >= code.length) {
         clearInterval(interval);
@@ -77,6 +77,7 @@ function run(code, curChar) {
       cells[pointer] = prompt("Enter a character to be used as an input. Only the first character will be read.").charCodeAt(0) || 0;
     break;
     case "]": // we don't actually need to worry about loop openings, only the closings :)
+      console.log(cells[pointer]);
       if (cells[pointer] !== 0) {
         var layer = 1;
         var tempChar = curChar;
